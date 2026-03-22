367ab411c9e8cb500fc8ce0a58a83f81ab0cbb25
Syed_Ikram <syed0ikram@gmail.com>
2026-03-22 21:29:29 +0530

    fixed the width of add chat

--- chat-client/src/components/AddChat.jsx
+++ chat-client/src/components/AddChat.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function AddChat() {
-  return <div className="min-h-screen min-w-screen bg-white">AddChat</div>;
+  return <div className="min-h-screen min-w-auto bg-white">AddChat</div>;
 }
 
 export default AddChat;
