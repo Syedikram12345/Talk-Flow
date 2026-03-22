3bbb5c8848e85d7d2be39a1b327db260c122f9a9
Syed_Ikram <syed0ikram@gmail.com>
2026-03-22 22:03:45 +0530

    fixed a small bug

--- chat-client/src/components/AddChat.jsx
+++ chat-client/src/components/AddChat.jsx
@@ -21,7 +21,7 @@ function AddChat() {
           <div className="flex flex-col">
             <label className="font-semibold mb-1">Unique ID</label>
             <input
-              type="number"
+              type="text"
               className="w-full px-3 py-2 bg-white text-black rounded-lg border border-gray-300 focus:border-black focus:ring-2 focus:ring-black outline-none"
               placeholder="Enter unique ID..."
             />
